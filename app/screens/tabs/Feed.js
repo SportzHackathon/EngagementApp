396cffbf3eea08f920ec1b831f0e3ac250f7ec56
crschriever <crschriever@gmail.com>
2018-10-16 23:12:22 -0400

    Add text blurb to feed

--- app/screens/tabs/Feed.js
+++ app/screens/tabs/Feed.js
@@ -3,8 +3,34 @@
  */
 
 import React, { Component } from "react";
-import { StyleSheet } from "react-native";
-import { Container, Header, Content, Footer, FooterTab, Button, Text, Icon } from "native-base";
+import { StyleSheet, ScrollView } from "react-native";
+import {
+    Container,
+    ListItem,
+    Header,
+    Content,
+    Footer,
+    FooterTab,
+    Button,
+    Text,
+    Icon
+} from "native-base";
+import TextFeedItem from "../../components/TextFeedItem";
+
+const articles = [
+    {
+        type: "text",
+        header: "Tech Wins 69-1!",
+        snippet: "What a great game and a damn good score.",
+        content: "Well bob, you saw what it said. What a great game and a damn good score."
+    },
+    {
+        type: "text",
+        header: "Beautiful day in the neighborhood",
+        snippet: "Bobby Dodd is looking awfully nice tonight mkay",
+        content: "Well bob, you saw what it said. Bobby Dodd is looking awfully nice tonight mkay"
+    }
+];
 
 export default class Feed extends Component {
     static navigationOptions = {
@@ -14,10 +40,19 @@ export default class Feed extends Component {
     render() {
         return (
             <Container>
-                <Text>Test Feed</Text>
+                <ScrollView scrollEnabled={true} bounces={true} style={styles.scrollView}>
+                    {articles.map((article, index) => (
+                        <TextFeedItem article={article} key={index} />
+                    ))}
+                </ScrollView>
             </Container>
         );
     }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    scrollView: {
+        marginLeft: 5,
+        marginRight: 5
+    }
+});
